08461523a5366fcf43a48760943747c8674beb93
Jack Hou <jackhou147@berkeley.edu>
2022-07-21 17:34:26 -0700

    styling

--- js/script.js
+++ js/script.js
@@ -3,9 +3,28 @@
 print("hello!")
 
 async function connectToAPI(){
-    fetch('http://localhost:1700/api/clustersentence/46908')
-        .then(response => response.json())
-        .then(data => console.log(data));
+    // fetch('http://localhost:1700/api/clustersentence/46908')
+    //     .then(response => response.json())
+    //     .then(IDs => {
+    //         print(IDs)
+    //         for(var i = 0; i < IDs.length; i++){
+    //             let id = IDs[i];
+    //             print(id);
+    //             fetch(`http://localhost:1700/api/sentencetofeedback/${id}`)
+    //                 .then(response => response.text().then(text => print(text)));
+    //         }
+    //     });
+
+    // fetch(`http://localhost:1700/api/sentencetofeedback/1022952`,
+    //     {
+    //         headers : {
+    //             'Content-Type': 'application/json',
+    //             'Accept': 'application/json'
+    //         }
+    //     })
+    //     .then(res => {
+    //         res.text().then(text => print(text));
+    //     });
 }
 
 connectToAPI();
